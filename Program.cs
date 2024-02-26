c27e0ed34ff4fe5f3be3f0cb0197e6463f225449
Plexukas <algirdasleonardas@gmail.com>
2024-02-26 17:32:44 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ContactManagementSystem{
+    
+    public class Person 
+    { // klase skirta zmoniu informacijos vientisumui
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        
+        public Person(string firstName, string lastName, string email)
+        { // konstruktorius
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+
+
+}

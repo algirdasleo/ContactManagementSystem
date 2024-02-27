f52108944ab61efae4f823882133813abc1761af
Plexukas <algirdasleonardas@gmail.com>
2024-02-27 14:20:52 +0200

    Added interface for managing contacts and class which implements it.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,64 @@
-﻿using System;
+﻿/*
+Simple contact management system.
+- Person class - first name, last name and email properties.
+- Contact class - first name, last name, email, phone number and address properties.
+- IContactManager interface - AddContact, UpdateContact, DeleteContact and SearchContacts methods.
+*/
+using System;
 
 namespace ContactManagementSystem{
-    
+    public interface IContactManager
+    {
+        void AddContact(Contact contact);
+        void UpdateContact(Contact contact);
+        void DeleteContact(string email);
+        List<Contact> SearchContacts(string keyword);
+    }
+    public class ContactManager : IContactManager
+    {
+        private List<Contact> _contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void UpdateContact(Contact contact)
+        {
+            for(int i = 0; i < _contacts.Count; i++)
+                if (_contacts[i].Email == contact.Email){
+                    _contacts[i] = contact;
+                    break;
+                }
+        }
+
+        public void DeleteContact(string email)
+        {
+            for (int i = 0; i < _contacts.Count; i++){
+                if (_contacts[i].Email == email){
+                    _contacts.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public List<Contact> SearchContacts(string keyword)
+        {
+            var searchResults = new List<Contact>();
+            for (int i = 0; i < _contacts.Count; i++){
+                Contact c = _contacts[i];
+                if (c.Address == keyword ||
+                    c.FirstName == keyword ||
+                    c.LastName == keyword ||
+                    c.PhoneNumber == keyword ||
+                    c.Email == keyword)
+                    {
+                        searchResults.Add(c);
+                    }
+            }
+            return searchResults;
+        }
+    }
     public class Person 
     { 
         public string FirstName { get; set; }
@@ -33,12 +90,23 @@ namespace ContactManagementSystem{
     {
         static void Main(string[] args)
         {
-            Person p = new Person("FirstName", "LastName", "pvz@email.com");
-            Contact c = new Contact("Vardas", "Pavarde", "pvz@email.com", "+37012345678", "Vasaros g. 5");
-            
-            Console.WriteLine($"Person: \n{p.FirstName} {p.LastName} {p.Email}");
-            Console.WriteLine($"Contact: \n{c.FirstName} {c.LastName}, {c.Email}. More info: {c.PhoneNumber}, {c.Address}");
+            ContactManager cm = new ContactManager();
+            Contact contact1 = new Contact("Vardas", "Pavarde", "pvz@email.com", "+37012345678", "Vasaros g. 5");
+            cm.AddContact(contact1);
 
+            Contact contact2 = new Contact("Vardas1", "Pavarde1", "pvz1@email.com", "+37012345678", "Vasaros g. 5");
+            cm.AddContact(contact2);
+
+            var contactList = cm.SearchContacts("+37012345678");
+            
+            if (contactList.Count != 0)
+            {
+                Console.WriteLine("Found these contacts by your keyword:");
+                foreach(var c in contactList)
+                    Console.WriteLine($"{c.FirstName} {c.LastName}, {c.Email}. More info: {c.PhoneNumber}, {c.Address}");
+            } else 
+                Console.WriteLine("No contacts found.");
+            
         }
     }
 }

98c913b642fd9ce1af9f49c7fbb2281f2e51bd6d
Plexukas <algirdasleonardas@gmail.com>
2024-02-27 12:14:52 +0200

    introduced contact class, which inherits the person class

--- Program.cs
+++ Program.cs
@@ -1,20 +1,44 @@
-﻿
+﻿using System;
+
 namespace ContactManagementSystem{
     
     public class Person 
-    { // klase skirta zmoniu informacijos vientisumui
+    { 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
 
-        
         public Person(string firstName, string lastName, string email)
-        { // konstruktorius
+        { 
             FirstName = firstName;
             LastName = lastName;
             Email = email;
         }
     }
+    
+    public class Contact : Person 
+    {
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
 
+        public Contact(string firstName, string lastName, string email, string phoneNumber, string address)
+            : base(firstName, lastName, email)
+        {
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("FirstName", "LastName", "pvz@email.com");
+            Contact c = new Contact("Vardas", "Pavarde", "pvz@email.com", "+37012345678", "Vasaros g. 5");
+            
+            Console.WriteLine($"Person: \n{p.FirstName} {p.LastName} {p.Email}");
+            Console.WriteLine($"Contact: \n{c.FirstName} {c.LastName}, {c.Email}. More info: {c.PhoneNumber}, {c.Address}");
 
+        }
+    }
 }
